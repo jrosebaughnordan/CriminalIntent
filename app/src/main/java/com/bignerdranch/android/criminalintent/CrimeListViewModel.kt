80160808263bcf65a21ab3903115e1cb3ab43e02
James Nordan <thejamesone@hotmail.com>
2020-05-29 23:27:55 -0400

    Criminal Intent end chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -10,7 +10,7 @@ class CrimeListViewModel : ViewModel() {
         for(i in 0 until 100) {
             val crime = Crime()
             crime.title = "Crime #$i"
-            crime.isSolved = 1 % 2 == 0
+            crime.isSolved = i % 2 == 0
             crimes += crime
         }
     }
