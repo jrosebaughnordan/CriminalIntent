a99e139fedc3f717668f3c0bc8a0cab4e8570876
James Nordan <thejamesone@hotmail.com>
2020-06-12 00:41:56 -0400

    end of chapter 16

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.bignerdranch.android.criminalintent.database.CrimeDatabase
 import com.bignerdranch.android.criminalintent.database.migration_1_2
+import java.io.File
 import java.util.*
 import java.util.concurrent.Executors
 import kotlin.IllegalStateException
@@ -22,6 +23,7 @@ class CrimeRepository private constructor (context: Context) {
 
     private val crimeDao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
+    private val filesDir = context.applicationContext.filesDir
 
     fun getCrimes() : LiveData<List<Crime>> = crimeDao.getCrimes()
 
@@ -37,6 +39,8 @@ class CrimeRepository private constructor (context: Context) {
             crimeDao.addCrime(crime)
         }
     }
+
+    fun getPhotoFile(crime: Crime): File = File(filesDir, crime.photoFileName)
 
     companion object{
         private var INSTANCE: CrimeRepository? = null
